10119d02f698826f8a69304c449cb4b0c1793efc
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-05-14 11:48:08 +0000

    0x04. Files manager Mandatory Task 1

---
+++ utils/db.js
@@ -0,0 +1,36 @@
+const mongodb = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const dbHost = process.env.DB_HOST || 'localhost';
+    const dbPort = process.env.DB_PORT || '27017';
+    const database = process.env.DB_DATABASE || 'files_manager';
+    this.connected = false;
+
+    this.dbClient = new mongodb.MongoClient(`mongodb://${dbHost}:${dbPort}/${database}`, { useUnifiedTopology: true });
+    this.dbClient.connect().then(() => {
+      this.connected = true;
+    }).catch((error) => console.log(error));
+  }
+
+  async isAlive() {
+    return this.connected;
+  }
+
+  async nbUsers() {
+    await this.dbClient.connect();
+    const db = this.dbClient.db();
+    const userCollections = await db.collection('users').countDocuments();
+    return userCollections;
+  }
+
+  async nbFiles() {
+    await this.dbClient.connect();
+    const db = this.dbClient.db();
+    const fileCollection = await db.collection('files').countDocuments();
+    return fileCollection;
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
